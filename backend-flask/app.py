1e4fd7e3dba1852b717ea043bb7775060e10dd62
vrushabhjv <vrushabhjv2@gmail.com>
2023-03-08 07:21:08 +0000

    slight change in app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -39,10 +39,13 @@ tracer = trace.get_tracer(__name__)
 # X-ray---------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='Backend-Flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 
 app = Flask(__name__)
+
+# X-ray---------
+XRayMiddleware(app, xray_recorder)
 
 #HoneyComb ------
 # Initialize automatic instrumentation with Flask
