f44fc86405b3a12ac44ff30b9c0343874e3b87d8
vrushabhjv <vrushabhjv2@gmail.com>
2023-03-08 10:38:31 +0000

    Implement cloudWatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -24,9 +24,22 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 # X-Ray
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+#from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# CloudWatch Logs----
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+#LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG)
+#console_handler = logging.StreamHandler()
+#cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+#LOGGER.addHandler(console_handler)
+#LOGGER.addHandler(cw_handler)
+#LOGGER.info("test log")
 
 #HoneyComb ------
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -37,15 +50,15 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 # X-ray---------
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='Backend-Flask', dynamic_naming=xray_url)
+#xray_url = os.getenv("AWS_XRAY_URL")
+#xray_recorder.configure(service='Backend-Flask', dynamic_naming=xray_url)
 
 
 
 app = Flask(__name__)
 
 # X-ray---------
-XRayMiddleware(app, xray_recorder)
+#XRayMiddleware(app, xray_recorder)
 
 #HoneyComb ------
 # Initialize automatic instrumentation with Flask
@@ -62,6 +75,13 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+#For CloudWatch logs
+#@app.after_request
+#def after_request(response):
+#    timestamp = strftime('[%Y-%b-%d %H:%M]')
+#    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#    return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
